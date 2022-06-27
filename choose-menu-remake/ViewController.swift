73894c6b85557a61a5cbf9df9703d4d6909d4508
ash <ogawa.sato01@gmail.com>
2022-06-27 22:34:46 +0900

    Initial Commit

---
+++ choose-menu-remake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  choose-menu-remake
+//
+//  Created by 小川慧 on 2022/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ choose-menu-remakeUITests/choose_menu_remakeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  choose_menu_remakeUITestsLaunchTests.swift
+//  choose-menu-remakeUITests
+//
+//  Created by 小川慧 on 2022/06/27.
+//
+
+import XCTest
+
+class choose_menu_remakeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
